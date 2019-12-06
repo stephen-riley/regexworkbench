1b00ad3a6571e3a8d81aa6cc841dd8db41004dca
Stephen Riley <jstephen.riley@yahoo.com>
2019-12-06 10:27:31 -0800

    complete layout change work (#4)
    * wip
    
    * got the sizing right on the TAs
    
    * completed textarea resizing

--- media/js/jquery-linedtextarea.js
+++ media/js/jquery-linedtextarea.js
@@ -1,4 +1,13 @@
 /**
+ * NOTE: This is a modified version of Alan Williamson's jquery-linedtextarea plugin.
+ *   The objective of the modifications is to use relative sizing for everything
+ *   so that dynamic resizing of the lined textareas is possible.  Also note that,
+ *   due to Chromium 66+ restrictions on reading external CSS rules from javascript,
+ *   you must specify any percentage-based widths on the textareas in their declaration
+ *   (eg. <textarea style="width:80%"></textarea>).
+ * 
+ *   Original attribution below:
+ * 
  * jQuery Lined Textarea Plugin 
  *   http://alan.blog-city.com/jquerylinedtextarea.htm
  *
@@ -34,116 +43,109 @@
  *   - 2010.01.06: Initial Release
  *
  */
-(function($) {
+(function ($) {
 
-	$.fn.linedtextarea = function(options) {
-		
+	$.fn.linedtextarea = function (options) {
+
 		// Get the Options
 		var opts = $.extend({}, $.fn.linedtextarea.defaults, options);
 		var LINEHEIGHT = 15;
-		
-		
+
 		/*
 		 * Helper function to make sure the line numbers are always
 		 * kept up to the current system
 		 */
-		var fillOutLines = function(codeLines, h, lineNo){
-			while ( (codeLines.height() - h ) <= 0 ){
-				if ( lineNo == opts.selectedLine )
+		var fillOutLines = function (codeLines, h, lineNo) {
+			while ((codeLines.height() - h) <= 0) {
+				if (lineNo == opts.selectedLine)
 					codeLines.append("<div class='lineno lineselect'>" + lineNo + "</div>");
 				else
 					codeLines.append("<div class='lineno'>" + lineNo + "</div>");
-				
+
 				lineNo++;
 			}
 			return lineNo;
 		};
-		
-		
+
 		/*
 		 * Iterate through each of the elements are to be applied to
 		 */
-		return this.each(function() {
+		return this.each(function () {
 			var lineNo = 1;
 			var textarea = $(this);
-			
+
 			/* Turn off the wrapping of as we don't want to screw up the line numbers */
 			textarea.attr("wrap", "off");
-			textarea.css({resize:'none'});
-			var originalTextAreaWidth	= textarea.outerWidth();
+			textarea.css({ resize: 'none' });
+			var originalTextareaCssWidth = textarea.get(0).style.width || (textarea.css('width') + 'px');
 
 			/* Wrap the text area in the elements we need */
 			textarea.wrap("<div class='linedtextarea'></div>");
-			var linedTextAreaDiv	= textarea.parent().wrap("<div class='linedwrap' style='width:" + originalTextAreaWidth + "px'></div>");
-			var linedWrapDiv 			= linedTextAreaDiv.parent();
-			
-			linedWrapDiv.prepend("<div class='lines' style='width:50px'></div>");
-			
-			var linesDiv	= linedWrapDiv.find(".lines");
-			linesDiv.height( textarea.height() + 6 );
-			
-			
-			/* Draw the number bar; filling it out where necessary */
-			linesDiv.append( "<div class='codelines'></div>" );
-			var codeLinesDiv	= linesDiv.find(".codelines");
-			lineNo = fillOutLines( codeLinesDiv, linesDiv.height(), 1 );
+			var linedTextAreaDiv = textarea.parent().wrap("<div class='linedwrap' style='width:" + originalTextareaCssWidth + "px'></div>");
+			var linedWrapDiv = linedTextAreaDiv.parent();
 
-			/* Move the textarea to the selected line */ 
-			if ( opts.selectedLine != -1 && !isNaN(opts.selectedLine) ){
-				var fontSize = parseInt( textarea.height() / (lineNo-2) );
-				var position = parseInt( fontSize * opts.selectedLine ) - (textarea.height()/2);
+			linedWrapDiv.prepend("<div class='lines' style='width:50px'></div>");
+			var linesComputedWidth = $('.lines').outerWidth();	// support overriding the gutter width
+
+			var linesDiv = linedWrapDiv.find(".lines");
+			linesDiv.height(textarea.height() + 6);
+
+			/* Draw the number bar; filling it out where necessary */
+			linesDiv.append("<div class='codelines'></div>");
+			var codeLinesDiv = linesDiv.find(".codelines");
+			lineNo = fillOutLines(codeLinesDiv, linesDiv.height(), 1);
+
+			/* Move the textarea to the selected line */
+			if (opts.selectedLine != -1 && !isNaN(opts.selectedLine)) {
+				var fontSize = parseInt(textarea.height() / (lineNo - 2));
+				var position = parseInt(fontSize * opts.selectedLine) - (textarea.height() / 2);
 				textarea[0].scrollTop = position;
 			}
 
-			
 			/* Set the width */
-			var sidebarWidth					= linesDiv.outerWidth();
-			var paddingHorizontal 		= parseInt( linedWrapDiv.css("border-left-width") ) + parseInt( linedWrapDiv.css("border-right-width") ) + parseInt( linedWrapDiv.css("padding-left") ) + parseInt( linedWrapDiv.css("padding-right") );
-			var linedWrapDivNewWidth 	= originalTextAreaWidth - paddingHorizontal;
-			var textareaNewWidth			= originalTextAreaWidth - sidebarWidth - paddingHorizontal - 20;
+			textarea.css('width', `calc(100% - ${linesComputedWidth * 1.5}px)`);
+			linedWrapDiv.css('width', originalTextareaCssWidth);
 
-			textarea.width( textareaNewWidth );
-			linedWrapDiv.width( linedWrapDivNewWidth );
-			
 			/* React to the scroll event */
 			var tid = null;
-			textarea.scroll( function(tn){
+			textarea.scroll(function (tn) {
 				if (tid === null) {
 					var that = this;
-					
+
 					// We use a timeout as to avoid appending/redrawing
 					// the div on every scroll event. This does add some latency
 					// before the right line number is displayed, but makes possible
 					// scrolling with a very high number of lines
-					tid = setTimeout( function() {
+					tid = setTimeout(function () {
 						codeLinesDiv.empty();
-						
+
 						// Calculare the line numbers to display
-						var domTextArea			= $(that)[0];
-						var scrollTop 			= domTextArea.scrollTop;
-						var firstLine 			= Math.floor((scrollTop / LINEHEIGHT) + 1);
+						var domTextArea = $(that)[0];
+						var scrollTop = domTextArea.scrollTop;
+						var firstLine = Math.floor((scrollTop / LINEHEIGHT) + 1);
 						var remainingScroll = (scrollTop / LINEHEIGHT) % 1;
 
-						fillOutLines( codeLinesDiv, linesDiv.height(), firstLine );
-						codeLinesDiv.css( {'margin-top': (-1*(remainingScroll*LINEHEIGHT)) + "px"} );
-						tid=null;
+						fillOutLines(codeLinesDiv, linesDiv.height(), firstLine);
+						codeLinesDiv.css({ 'margin-top': (-1 * (remainingScroll * LINEHEIGHT)) + "px" });
+						tid = null;
 					}, 150);
 				}
 			});
 
 
 			/* Should the textarea get resized outside of our control */
-			textarea.resize( function(tn){
-				var domTextArea	= $(this)[0];
-				linesDiv.height( domTextArea.clientHeight + 6 );
+			textarea.resize(function (tn) {
+				var domTextArea = $(this)[0];
+				linesDiv.height(domTextArea.clientHeight + 6);
 			});
 
 		});
 	};
 
-  // default options
-  $.fn.linedtextarea.defaults = {
-  	selectedLine: -1,
-  	selectedClass: 'lineselect'
-  };
+	// default options
+	$.fn.linedtextarea.defaults = {
+		selectedLine: -1,
+		selectedClass: 'lineselect'
+	};
+
 })(jQuery);
